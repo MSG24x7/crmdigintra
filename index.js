2012f65117a2674f406f214d239e3f757e9918c8
MSG24x7 <connect@msg24x7.com>
2024-10-16 11:13:52 +0530

    solved error of index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv").config();
 const dbConnect = require("./dbconnect.js");
 const userModel = require("./models/userModel.js");
-const WhatsAppMessage = require("./models/whatsappModel.js");
+const WhatsAppMessage = require("./models/WhatsappModel.js");
 const app = express();
 
 app.use(express.json());
